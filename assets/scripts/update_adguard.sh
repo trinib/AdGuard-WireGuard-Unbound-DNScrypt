6ee00f6d30d5a1c8e56511fde5c04cefff4029d9
trinib <watchtv4ever@gmail.com>
2022-04-29 03:01:37 -0400

    Added update script for AdGuard Home

--- assets/scripts/update_adguard.sh
+++ assets/scripts/update_adguard.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-echo ' ─ Downloading package...'
+echo '  ─  Downloading package...'
 wget -o '/tmp/AdGuardHome_linux_arm64.tar.gz'\
     'https://static.adguard.com/adguardhome/release/AdGuardHome_linux_arm64.tar.gz'
 echo 'SUCCESS'
 
-echo ' ─  Changing directory...'
+echo '  ─  Changing directory...'
 cd /opt/AdGuardHome
 echo 'SUCCESS'
 
-echo ' ─  Stopping AdGuard Home...'
+echo '  ─  Stopping AdGuard Home...'
 sudo ./AdGuardHome -s stop
 echo 'SUCCESS'
 
-echo ' ─  Backing up data...'
+echo '  ─  Backing up data...'
 mkdir -p /home/pi/my-agh-backup
 cp -r ./AdGuardHome.yaml ./data /home/pi/my-agh-backup/
 echo '[*] SUCCESS'
 
-echo ' ─  Unpacking AdGuard Home archive...'
+echo '  ─  Unpacking AdGuard Home archive...'
 tar -C /tmp/ -f /home/pi/AdGuardHome_linux_arm64.tar.gz -x -v -z
 echo 'SUCCESS'
 
-echo ' ─  Replacing the old AdGuard Home executable file with the new one....'
+echo '  ─  Replacing the old AdGuard Home executable file with the new one....'
 sudo cp /tmp/AdGuardHome/AdGuardHome /opt/AdGuardHome/AdGuardHome
 echo 'SUCCESS'
 
-echo ' ─  Restarting AdGuard Home....'
+echo '  ─  Restarting AdGuard Home....'
 sudo ./AdGuardHome -s start
 echo 'SUCCESS'
 echo
